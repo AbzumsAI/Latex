82bcf9bda98ef7c9489355f1964cfdd7eec1058d
ImanM02 <imanm1381@gmail.com>
2026-07-21 13:08:13 -0700

    Add template file list

---
+++ scripts/list-template-files.ps1
@@ -0,0 +1,12 @@
+﻿$root = Split-Path -Parent $PSScriptRoot
+$template = Join-Path $root 'Template'
+
+Get-ChildItem -LiteralPath $template -Recurse -File |
+    Where-Object { $_.FullName -notlike '*\build\*' } |
+    ForEach-Object {
+        [pscustomobject]@{
+            File = $_.FullName.Substring($root.Length + 1)
+            Size = $_.Length
+        }
+    } |
+    Sort-Object File
