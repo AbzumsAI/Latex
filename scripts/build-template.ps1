e3faecbf9249ebff7259e69d8348e3d7fc2ee971
ImanM02 <imanm1381@gmail.com>
2026-07-21 13:08:12 -0700

    Add template build script

---
+++ scripts/build-template.ps1
@@ -0,0 +1,10 @@
+﻿$root = Split-Path -Parent $PSScriptRoot
+$template = Join-Path $root 'Template'
+
+Push-Location -LiteralPath $template
+try {
+    latexmk -pdfxe -interaction=nonstopmode -halt-on-error -outdir=build HW.tex
+    exit $LASTEXITCODE
+} finally {
+    Pop-Location
+}
